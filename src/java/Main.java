d99cf456abcfc0a00ab4f8ea57bd2c7c6d8ac6e3
rrrguez <alu0101394763@ull.edu.es>
2024-03-17 18:31:39 +0000

    Added input fields in frame. Connection to server is now working! The program can detect if the user exists or not, and if the password is correct or not.

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,4 @@
-
+/**
 import interfaz.VentanaInicio;
 
 import java.sql.Connection;
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         // Datos de conexión
-        String url = "jdbc:mysql://LAPTOP-QKDBRERA:3306/OLYMPULL_DB?useSSL=false";
+        String url = "jdbc:mysql://192.168.56.1:3306/OLYMPULL_DB";
         String usuario = "rrrguez";
         String contrasea = "adminPassword"; // Reemplaza 'CONTRASEÑA' con la contraseña real
 
@@ -32,7 +32,6 @@ public class Main {
 }
 
 
-/**
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -72,3 +71,75 @@ public class Main {
     }
 }
  */
+
+import com.jcraft.jsch.*;
+import interfaz.VentanaInicio;
+
+import java.sql.*;
+
+public class Main {
+    public static Connection conn;
+    public static void main(String[] args) {
+        // Valores para conexión a MV remota
+        String sshHost = "10.6.130.204";
+        String sshUser = "usuario";
+        String sshPassword = "Usuario";
+        int sshPort = 22; // Puerto SSH por defecto
+        int localPort = 3307; // Puerto local para el túnel SSH
+        String remoteHost = "localhost"; // La conexión MySQL se hará desde la máquina remota
+        int remotePort = 3306; // Puerto MySQL en la máquina remota
+
+        try {
+            // Conexión SSH a la MV remota
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(sshUser, sshHost, sshPort);
+            session.setPassword(sshPassword);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            // Debugger
+            System.out.println("Conexión con la máquina establecida");
+
+            // Abrir un túnel SSH al puerto MySQL en la máquina remota
+            session.setPortForwardingL(localPort, remoteHost, remotePort);
+
+            // Conexión a MySQL a través del túnel SSH
+            String dbUrl = "jdbc:mysql://localhost:" + localPort + "/OLYMPULL_DB";
+            String dbUser = "root";
+            String dbPassword = "root";
+            conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+            // Debugger
+            System.out.println("Conexión con MySQL establecida");
+
+/**
+            // Consulta SQL de prueba
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO PRUEBA (COLUMNA_1, COLUMNA_2) VALUES (10, 'ADIOS')";
+            int rowsAffected = stmt.executeUpdate(sql);
+
+            // Verificar el número de filas afectadas
+            if (rowsAffected > 0) {
+                System.out.println("Inserción exitosa. Se insertaron " + rowsAffected + " filas.");
+            } else {
+                System.out.println("La inserción no tuvo éxito. No se insertaron filas.");
+            }
+
+            // Cerrar recursos
+            stmt.close();
+ */
+            // Cerrar conexión a MySQL
+            conn.close();
+
+            // Cerrar la conexión SSH
+            session.disconnect();
+
+            // Abrir ventana principal
+            VentanaInicio ventana = new VentanaInicio();
+
+
+        } catch (JSchException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/java/interfaz/VentanaInicio.java
+++ src/java/interfaz/VentanaInicio.java
@@ -1,20 +1,32 @@
 package interfaz;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 import javax.swing.*;
 import java.awt.*;
 
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 import javax.swing.ImageIcon;
 
 public class VentanaInicio extends JFrame {
     JPanel logoPanel;
+    JPanel credentialsPanel;
+    JPanel loginPanel;
     JLabel olympullLogo;
+    JLabel userTag;
+    JLabel passwordTag;
+    JTextField userField;
+    JPasswordField passwordField;
+    JButton loginButton;
 
     public VentanaInicio() {
-        setSize(500, 500);
-        getContentPane().setLayout(new BorderLayout());
+        setSize(500, 250);
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Bienvenido a OlympULL");
         logoPanel = new JPanel();
@@ -23,11 +35,101 @@ public class VentanaInicio extends JFrame {
         // Add logo to the label that will be on the top of the window
         ImageIcon logo = new ImageIcon("images/logo olympull.png");
         Image originalIcon = logo.getImage();
-        Image escalatedLogo = originalIcon.getScaledInstance(536, 112, Image.SCALE_SMOOTH);
+        Image escalatedLogo = originalIcon.getScaledInstance(268, 56, Image.SCALE_SMOOTH);
         logo = new ImageIcon(escalatedLogo);
         olympullLogo = new JLabel(logo);
-        logoPanel.add(olympullLogo, BorderLayout.CENTER);
-        add(logoPanel);
+        logoPanel.add(olympullLogo);
+        add(logoPanel, BorderLayout.NORTH);
+
+        // Configure credentials panel
+        userTag = new JLabel("Usuario");
+        passwordTag = new JLabel("Contraseña");
+
+        userField = new JTextField();
+        passwordField = new JPasswordField();
+
+        credentialsPanel = new JPanel();
+        credentialsPanel.setBackground(new Color(249, 233, 93));
+        credentialsPanel.setLayout(new GridLayout(2, 2));
+        credentialsPanel.add(userTag);
+        credentialsPanel.add(userField);
+        credentialsPanel.add(passwordTag);
+        credentialsPanel.add(passwordField);
+
+        add(credentialsPanel);
+
+        loginPanel = new JPanel();
+        loginPanel.setBackground(new Color(249, 233, 93));
+        loginButton = new JButton("Iniciar sesión");
+        loginPanel.add(loginButton);
+        add(loginPanel);
+
+        // Realizar la consulta SQL para comprobar la existencia del usuario
+        // Valores para conexión a MV remota
+        String sshHost = "10.6.130.204";
+        String sshUser = "usuario";
+        String sshPassword = "Usuario";
+        int sshPort = 22; // Puerto SSH por defecto
+        int localPort = 3307; // Puerto local para el túnel SSH
+        String remoteHost = "localhost"; // La conexión MySQL se hará desde la máquina remota
+        int remotePort = 3306; // Puerto MySQL en la máquina remota
+
+        // Action listener
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Lógica para comprobar la existencia de un usuario
+                if (userField.getText() != null && passwordField.getText() != null) {
+                    try {
+                        // Conexión SSH a la MV remota
+                        JSch jsch = new JSch();
+                        Session session = jsch.getSession(sshUser, sshHost, sshPort);
+                        session.setPassword(sshPassword);
+                        session.setConfig("StrictHostKeyChecking", "no");
+                        session.connect();
+
+                        // Debugger
+                        System.out.println("Conexión con la máquina establecida");
+
+                        // Abrir un túnel SSH al puerto MySQL en la máquina remota
+                        session.setPortForwardingL(localPort, remoteHost, remotePort);
+
+                        // Conexión a MySQL a través del túnel SSH
+                        String dbUrl = "jdbc:mysql://localhost:" + localPort + "/OLYMPULL_DB";
+                        String dbUser = "root";
+                        String dbPassword = "root";
+                        Connection conn;
+                        conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+                        // Debugger
+                        Statement stmt = conn.createStatement();
+
+                        // Ejecutar la consulta SQL
+                        String sql = "SELECT NOMBRE_USUARIO FROM T_USUARIOS WHERE NOMBRE_USUARIO = " + "'" + userField.getText() + "'";
+                        ResultSet rs = stmt.executeQuery(sql);
+
+                        if (rs.next()) {
+                            System.out.println("Existe el usuario");
+                            String id = rs.getString("NOMBRE_USUARIO");
+                            String sql2 = "SELECT PASSWORD FROM T_USUARIOS WHERE NOMBRE_USUARIO = " + "'" + id + "' AND PASSWORD = " + "'" + passwordField.getText() + "'";
+                            rs = stmt.executeQuery(sql2);
+                            if (rs.next()) {
+                                System.out.println("Contraseña correcta. Iniciando sesión...");
+                            } else {
+                                System.out.println("Contraseña incorrecta. Pruebe otra vez.");
+                            }
+                        } else {
+                            System.out.println("No existe el usuario");
+                        }
+
+                        conn.close();
+                        session.disconnect();
+                    } catch (JSchException | SQLException i) {
+                        i.printStackTrace();
+                    }
+                }
+            }
+        });
         this.setVisible(true);
     }
 
